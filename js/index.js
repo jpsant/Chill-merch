dbc6bcdae5620d0ddb3423332fe7e240a349457d
João Paulo Santana <j.paaulinho@gmail.com>
2018-11-19 17:47:26 -0300

    responsividade (update)
    inicio da responsividade da navbar

--- js/index.js
+++ js/index.js
@@ -8,16 +8,17 @@ $(function () {
     $(document).scroll(function () {
         var $nav = $(".fixed-top");
         var $bar = $(".bar");
-        var $brand = $(".navbar-brand")
+        var $brand = $(".navbar-brand");
+        var $item = $(".nav-item");
         $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
         $bar.toggleClass('black', $(this).scrollTop() > $nav.height());
         $brand.toggleClass('black', $(this).scrollTop() > $nav.height());
+        $item.toggleClass('black', $(this).scrollTop() > $nav.height());
     });
 });
 
 function openNav() {
     document.getElementById("mySidenav").style.width = "300px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
 function closeNav() {
