c74a2d38cef0413765d24301f3b3392661f50829
João Paulo Santana <j.paaulinho@gmail.com>
2018-11-27 17:18:44 -0300

    main
    finalização da versão desktop da main page

--- js/index.js
+++ js/index.js
@@ -25,12 +25,3 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
 
-new Glider(document.querySelector('.glider'), {
-    slidesToShow: 2,
-    draggable: true,
-    dots: '#dots',
-    arrows: {
-        prev: '.glider-prev',
-        next: '.glider-next'
-    }
-});
