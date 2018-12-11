4d22fdef0709efe9726eff9307b071e81b2ed7f7
João Paulo Santana <j.paaulinho@gmail.com>
2018-12-11 17:07:23 -0300

    card switch
    transição entre os cards de log-in e cadastro concluída!

--- js/login.js
+++ js/login.js
@@ -4,6 +4,9 @@ $("#switch").click(function() {
     $('.body').toggleClass('switch-bg');
     $('.login').toggleClass('login-animation');
     $('.signup').toggleClass('signup-animation');
+    $('.login').toggleClass('login-animation-back');
+    $('.signup').toggleClass('signup-animation-back');
+
     setTimeout(function() {
       $('.signup').toggleClass('back');
     }, 600);
@@ -13,8 +16,9 @@ $("#switch").click(function() {
     $('.login-text').toggleClass('hide');
     $('.signup-text').toggleClass('hide');
     $('.body').toggleClass('switch-bg');
-    $('.login').removeClass('login-animation');
-    $('.signup').removeClass('signup-animation');
+    $('.login').toggleClass('login-animation-back');
+    $('.signup').toggleClass('signup-animation-back');
+    
     setTimeout(function() {
       $('.signup').toggleClass('back');
     }, 600);
