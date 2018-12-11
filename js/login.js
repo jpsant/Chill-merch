b03363647b251aa1c4ca1053368964af3eb2e483
João Paulo Santana <j.paaulinho@gmail.com>
2018-12-11 16:04:44 -0300

    cards animation
    correção das animações dos cards

--- js/login.js
+++ js/login.js
@@ -1,7 +1,20 @@
 $("#switch").click(function() {
+    $('.login-text').toggleClass('hide');
+    $('.signup-text').toggleClass('hide');
     $('.body').toggleClass('switch-bg');
     $('.login').toggleClass('login-animation');
     $('.signup').toggleClass('signup-animation');
+    setTimeout(function() {
+      $('.signup').toggleClass('back');
+    }, 600);
+  });
+
+  $("#switch2").click(function() {
+    $('.login-text').toggleClass('hide');
+    $('.signup-text').toggleClass('hide');
+    $('.body').toggleClass('switch-bg');
+    $('.login').removeClass('login-animation');
+    $('.signup').removeClass('signup-animation');
     setTimeout(function() {
       $('.signup').toggleClass('back');
     }, 600);
