12c201132dfb054ec914173d07ff22ed70010112
João Paulo Santana <j.paaulinho@gmail.com>
2018-12-11 14:10:23 -0300

    update!
    remoção do arquivo 'GliderJS' e correção da animação de switch dos card de login e cadastro. Tudo está funcionando.

--- js/login.js
+++ js/login.js
@@ -1,5 +1,8 @@
 $("#switch").click(function() {
     $('.body').toggleClass('switch-bg');
-    $('.login').toggleClass('login-card');
-    
+    $('.login').toggleClass('login-animation');
+    $('.signup').toggleClass('signup-animation');
+    setTimeout(function() {
+      $('.signup').toggleClass('back');
+    }, 600);
   });
