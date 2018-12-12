2bfc0e6a24e201f6f07dcd55bde6984b48f0ca06
João Paulo Santana <j.paaulinho@gmail.com>
2018-12-12 17:30:01 -0300

    longin/signuo
    anmação da troca de cards funcionando perfeitamente!

--- js/login.js
+++ js/login.js
@@ -2,10 +2,10 @@ $("#switch").click(function() {
     $('.login-text').toggleClass('hide');
     $('.signup-text').toggleClass('hide');
     $('.body').toggleClass('switch-bg');
-    $('.login').toggleClass('login-animation');
-    $('.signup').toggleClass('signup-animation');
-    $('.login').toggleClass('login-animation-back');
-    $('.signup').toggleClass('signup-animation-back');
+    $(".login").removeClass("login-animation-back");
+    $(".signup").removeClass("signup-animation-back");
+    $(".login").toggleClass("login-animation");
+    $(".signup").toggleClass("signup-animation");
 
     setTimeout(function() {
       $('.signup').toggleClass('back');
@@ -16,8 +16,10 @@ $("#switch").click(function() {
     $('.login-text').toggleClass('hide');
     $('.signup-text').toggleClass('hide');
     $('.body').toggleClass('switch-bg');
-    $('.login').toggleClass('login-animation-back');
-    $('.signup').toggleClass('signup-animation-back');
+    $(".login").removeClass("login-animation");
+    $(".signup").removeClass("signup-animation");
+    $(".login").toggleClass("login-animation-back");
+    $(".signup").toggleClass("signup-animation-back");
     
     setTimeout(function() {
       $('.signup').toggleClass('back');
